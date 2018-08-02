75905af6168febf3222e29c7177d08e12c011234
Vlad <zilkov@hotmail.com>
2018-08-02 10:44:32 +0300

    Calculating temperature of micro or mosfet. NON STABLE!!!

---
+++ ElectricCalculators_winforms/TemperatureCalculatings/Form1.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace TemperatureCalculatings
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox_lowpass_filter = new System.Windows.Forms.PictureBox();
+            this.pictureBox_temperature = new System.Windows.Forms.PictureBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_lowpass_filter)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_temperature)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Temperature",
+            "Low-pass filter"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 32);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(174, 24);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "Temperature";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Choose calculator";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox_lowpass_filter
+            // 
+            this.pictureBox_lowpass_filter.Image = global::TemperatureCalculatings.Properties.Resources._250px_1st_Order_Lowpass_Filter_RC_svg;
+            this.pictureBox_lowpass_filter.Location = new System.Drawing.Point(457, 32);
+            this.pictureBox_lowpass_filter.Name = "pictureBox_lowpass_filter";
+            this.pictureBox_lowpass_filter.Size = new System.Drawing.Size(447, 286);
+            this.pictureBox_lowpass_filter.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox_lowpass_filter.TabIndex = 3;
+            this.pictureBox_lowpass_filter.TabStop = false;
+            this.pictureBox_lowpass_filter.Visible = false;
+            // 
+            // pictureBox_temperature
+            // 
+            this.pictureBox_temperature.Enabled = false;
+            this.pictureBox_temperature.Image = global::TemperatureCalculatings.Properties.Resources.junction_temperature_life_indium_thermal_interface_material_2;
+            this.pictureBox_temperature.Location = new System.Drawing.Point(457, 9);
+            this.pictureBox_temperature.Name = "pictureBox_temperature";
+            this.pictureBox_temperature.Size = new System.Drawing.Size(447, 309);
+            this.pictureBox_temperature.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox_temperature.TabIndex = 2;
+            this.pictureBox_temperature.TabStop = false;
+            this.pictureBox_temperature.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 114);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(112, 22);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Visible = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(11, 162);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(112, 22);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(9, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Label2";
+            this.label2.Visible = false;
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(8, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Label3";
+            this.label3.Visible = false;
+            this.label3.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(7, 191);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 20);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Label4";
+            this.label4.Visible = false;
+            this.label4.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(11, 214);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(112, 22);
+            this.textBox4.TabIndex = 4;
+            this.textBox4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(7, 236);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 20);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Label5";
+            this.label5.Visible = false;
+            this.label5.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(11, 259);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(112, 22);
+            this.textBox5.TabIndex = 4;
+            this.textBox5.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(7, 284);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 20);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Label6";
+            this.label6.Visible = false;
+            this.label6.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(11, 307);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(112, 22);
+            this.textBox6.TabIndex = 4;
+            this.textBox6.Visible = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(9, 332);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 20);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Label7";
+            this.label7.Visible = false;
+            this.label7.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(11, 355);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(112, 22);
+            this.textBox7.TabIndex = 4;
+            this.textBox7.Visible = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(129, 503);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(98, 21);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.Visible = false;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(10, 503);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 29);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(10, 403);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(112, 22);
+            this.textBox8.TabIndex = 4;
+            this.textBox8.Visible = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(6, 380);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(59, 20);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "Label8";
+            this.label8.Visible = false;
+            this.label8.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(452, 464);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(77, 25);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "Label9";
+            this.label9.Visible = false;
+            this.label9.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(452, 435);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(89, 25);
+            this.label10.TabIndex = 1;
+            this.label10.Text = "Label10";
+            this.label10.Visible = false;
+            this.label10.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(913, 645);
+            this.Controls.Add(this.pictureBox_lowpass_filter);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.pictureBox_temperature);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Calculators";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_lowpass_filter)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_temperature)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox_temperature;
+        private System.Windows.Forms.PictureBox pictureBox_lowpass_filter;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ ElectricCalculators_winforms/TemperatureCalculatings/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TemperatureCalculatings
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "Temperature")
+            {
+                pictureBox_lowpass_filter.Visible = false;
+                pictureBox_temperature.Visible = true;
+
+                label2.Text = "Max power dissipation (P):";
+                label3.Text = "Ambient temperature (Ta):";
+                label4.Text = "Thermal resistance, Junction-to-Ambient (Rja):";
+                label5.Text = "Thermal resistance, Radiator-to_Atmosphere(Rra):";
+                label6.Text = "Thermal resistance, Junction-to-Case (Rjc):";
+                label7.Text = "Thermal resistance, Case-to-Radiator(average 1)(Rcr):";
+                
+                label2.Visible = true;
+                label3.Visible = true;
+                label4.Visible = true;
+                //label5.Visible = true;
+                //label6.Visible = true;
+                //label7.Visible = true;
+                label10.Visible = true;
+                label10.Text = "Tj = P * Rja + Ta";
+
+                textBox2.Visible = true;
+                textBox3.Visible = true;
+                textBox4.Visible = true;
+                //textBox5.Visible = true;
+                //textBox6.Visible = true;
+                //textBox7.Visible = true;
+                //textBox8.Visible = true;
+
+                button1.Text = "Calculate";
+                button1.Visible = true;
+
+                checkBox1.Visible = true;
+                checkBox1.Text = "Radiator ENABLE";
+            }
+            else if(comboBox1.Text == "Low-pass filter")
+            {
+                pictureBox_temperature.Visible = false;
+                pictureBox_lowpass_filter.Visible = true;
+            }
+        }
+        private void reset_values() {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(comboBox1.Text == "Temperature")
+            {
+                label9.Visible = true;
+                if(checkBox1.CheckState == CheckState.Unchecked)
+                {
+                    try
+                    {
+                        double TempJunction;
+                        textBox2.Text = textBox2.Text.Replace('.', ',');
+                        textBox7.Text = textBox7.Text.Replace('.', ',');
+                        textBox4.Text = textBox4.Text.Replace('.', ',');
+                        TempJunction = Convert.ToDouble(textBox2.Text) * Convert.ToDouble(textBox4.Text) + Convert.ToDouble(textBox3.Text);
+                        label9.Text = "Crystal temperature is " + Convert.ToString(TempJunction)+" C";
+                    }
+                    catch (FormatException)
+                    {
+                        label9.Text = "Wrong input value";
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        double TempJunction;
+                        textBox2.Text = textBox2.Text.Replace('.', ',');
+                        textBox3.Text = textBox3.Text.Replace('.', ',');
+                        textBox5.Text = textBox5.Text.Replace('.', ',');
+                        textBox6.Text = textBox6.Text.Replace('.', ',');
+                        textBox7.Text = textBox7.Text.Replace('.', ',');
+
+                        TempJunction = Convert.ToDouble(textBox2.Text) *
+                            (Convert.ToDouble(textBox5.Text) + Convert.ToDouble(textBox6.Text) + Convert.ToDouble(textBox7.Text))
+                            + Convert.ToDouble(textBox3.Text);
+                        label9.Text = Convert.ToString(TempJunction) + " C";
+                    }
+                    catch (FormatException) {
+                        label9.Text = "Wrong input value";
+                    }
+                }
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.CheckState == CheckState.Checked)
+            {
+                label10.Text = "Tj = P * (Rjc + Rcr + Rra) + Ta";
+
+                label4.Visible = false;
+                textBox4.Visible = false;
+                label5.Visible = true;
+                label6.Visible = true;
+                label7.Visible = true;
+                textBox5.Visible = true;
+                textBox6.Visible = true;
+                textBox7.Visible = true;
+            }
+            else if(checkBox1.CheckState == CheckState.Unchecked)
+            {
+                label10.Text = "Tj = P * Rja + Ta";
+                label4.Visible = true;
+                textBox4.Visible = true;
+                label5.Visible = false;
+                label6.Visible = false;
+                label7.Visible = false;
+                textBox5.Visible = false;
+                textBox6.Visible = false;
+                textBox7.Visible = false;
+            }
+        }
+    }
+}
